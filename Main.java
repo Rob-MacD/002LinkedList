11df618ce5238b9755bb14f6757e5a180bcd33b2
Rob-MacD <rob.m.macdonald@gmail.com>
2022-05-08 23:45:42 -0300

    can use multiple data types including another linkedlist. Cleaned up linkedList 'add' functions.

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args){
-    linkedList list = new linkedList();
-    list = linkedList.addTail(list, 5);
-    list = linkedList.addTail(list, 7);
-    list = linkedList.addHead(list, 1);
-    list = linkedList.addHead(list, 3);
-    System.out.println("Number of Nodes: " + linkedList.getCount());
-    
+    LinkedList list = new LinkedList();
+    LinkedList test = new LinkedList();
+    list = LinkedList.addTail(list, "Hello");
+    list = LinkedList.addTail(list, "World!");
+    list = LinkedList.addHead(list, test);
+    list = LinkedList.addHead(list, 3);
+    System.out.println("Number of Nodes: " + LinkedList.getCount());
   }
 }
--- linkedList.java
+++ linkedList.java
@@ -1,36 +1,34 @@
-public class linkedList {
+public class LinkedList {
   private Node head;
   private Node tail;
   private static int count;
 
   public static int getCount() { return count; }
 
-  public linkedList(){
+  public LinkedList(){
     head = null;
     tail = null;
     count = 0;
   }
 
-  public static linkedList addHead(linkedList list, int value) {
+  public static LinkedList addHead(LinkedList list, Object value) {
     Node newNode = new Node(value);
-    if (list.head != null){
+    if (list.head != null)
       list.head.prev = newNode;
-    } else {
+    else 
       list.tail = newNode;
-    }
     newNode.next = list.head;
     list.head = newNode;
     count++;
     return list;
   }
 
-    public static linkedList addTail(linkedList list, int value) {
+    public static LinkedList addTail(LinkedList list, Object value) {
     Node newNode = new Node(value);
     if (list.tail != null)
       list.tail.next = newNode;
-    if (list.head == null) {
+    if (list.head == null) 
       list.head = newNode;
-    }
     newNode.prev = list.tail;
     list.tail = newNode;
     count++;
