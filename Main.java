6813f350f5b0e21381f5c4e454440705d1a9cdfb
Rob-MacD <rob.m.macdonald@gmail.com>
2022-05-11 13:14:49 -0300

    Added addHead Added addTail Added getValue (incomplete).

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
 public class Main {
   public static void main(String[] args){
     LinkedList list = new LinkedList();
-    LinkedList test = new LinkedList();
-    list = LinkedList.addTail(list, "Hello");
-    list = LinkedList.addTail(list, "World!");
-    list = LinkedList.addHead(list, test);
-    list = LinkedList.addHead(list, 3);
+    // list = LinkedList.addTail(list, "Hello");
+    // list = LinkedList.addTail(list, "World!");
+    // list = LinkedList.addHead(list, test);
+    // list = LinkedList.addHead(list, 3);
+    list = LinkedList.addAt(list, 2, 0);
+    list = LinkedList.addAt(list, 0, 1);
+    list = LinkedList.addAt(list, 1, 2);
+    list = LinkedList.addAt(list, 1, 3);
+    list = LinkedList.addAt(list, 2, 4);
+    list = LinkedList.addAt(list, 0, 5);
+    list = LinkedList.addAt(list, 4, 6);
     System.out.println("Number of Nodes: " + LinkedList.getCount());
+    System.out.println("Value of node 2: " + (int)LinkedList.getValue(list, 2));
   }
 }
--- linkedList.java
+++ linkedList.java
@@ -1,18 +1,28 @@
 public class LinkedList {
-  private Node head;
-  private Node tail;
+  private Node<?> head;
+  private Node<?> tail;
   private static int count;
-
-  public static int getCount() { return count; }
 
   public LinkedList(){
     head = null;
     tail = null;
     count = 0;
   }
+  
+  public class Node<T> {
+  T data;
+  Node<T> next;
+  Node<T> prev;
+
+  public Node(T d){
+    this.data = d;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
   public static LinkedList addHead(LinkedList list, Object value) {
-    Node newNode = new Node(value);
+    Node newNode = list.new Node(value);
     if (list.head != null)
       list.head.prev = newNode;
     else 
@@ -23,8 +33,8 @@ public class LinkedList {
     return list;
   }
 
-    public static LinkedList addTail(LinkedList list, Object value) {
-    Node newNode = new Node(value);
+  public static LinkedList addTail(LinkedList list, Object value) {
+    Node newNode = list.new Node(value);
     if (list.tail != null)
       list.tail.next = newNode;
     if (list.head == null) 
@@ -34,4 +44,50 @@ public class LinkedList {
     count++;
     return list;
   }
+
+  public static LinkedList addAt(LinkedList list, int index, Object value) {
+    if (index == 0)
+      addHead(list, value);
+    else if (index >= getCount())
+      addTail(list, value);
+    else {
+      Node newNode = list.new Node(value);
+      Node current = list.head;
+      for(int i = 0; i < index; i++)
+        current = current.next;
+      //Set new node to point to proper nodes
+      newNode.prev = current.prev;
+      newNode.next = current;
+      //Modify adjacent nodes:
+      current.prev.next = newNode;
+      current.prev = newNode;
+      count++;
+    }
+    return list;
+  }
+
+  public static LinkedList removeHead(LinkedList list){
+    return list;
+  }
+
+  public static LinkedList removeTail(LinkedList list){
+    return list;
+  }
+
+  public static LinkedList removeAt(LinkedList list){
+    return list;
+  }
+
+  public static Object getValue(LinkedList list, int index){
+    Node<?> current = list.head;
+    for(int i = 0; i > index; i++) {
+      current = current.next;
+    }
+    return current.data;
+  }
+
+  public static int getCount() {
+    return count;
+  }
+
 }
